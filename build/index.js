a5338ba78e706b3cf3f67eb12d868aff5a26e602
viettu2k <doanviettu2k@gmail.com>
2022-06-24 15:26:17 +0700

    concurrently compilation and execution

--- build/index.js
+++ build/index.js
@@ -3,3 +3,4 @@ console.log('hi there');
 const consoleLogSomeThing = () => {
     console.log('some thing');
 };
+consoleLogSomeThing();
--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,5 @@ console.log('hi there');
 const consoleLogSomeThing = () => {
   console.log('some thing');
 };
+
+consoleLogSomeThing();
